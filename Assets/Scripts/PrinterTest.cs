056622a4f52f3d5493566746f6a4a952788cb621
Baekdu2009 <cik7353@gmail.com>
2024-09-10 00:13:42 +0900

    0910_1210; feat. 프린터 테스트

--- Assets/Scripts/PrinterTest.cs
+++ Assets/Scripts/PrinterTest.cs
@@ -1,24 +1,29 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PrinterTest : MonoBehaviour
 {
     // 움직임 제한 범위 (x, y, z) = (-0.5 ~ 0.5, 0.6 ~ 1.15, -0.4 ~ 0.4)
     public GameObject nozzle;
-    Transform nozzleOriginPos;
-    public float nozzleSpeed = 2f;
-    [Range(-0.5f, 0.5f)]
-    float xPosRange;
+    Vector3 nozzleOriginPos;
+    Vector3 pos;
+    public float nozzleSpeed;
+    float xRange = 0.5f;
+    float yMin = 0.6f;
+    float yMax = 1.15f;
+    float zRange = 0.4f;
 
     void Start()
     {
-        nozzleOriginPos = nozzle.transform;
+        nozzleOriginPos = nozzle.transform.position;
+        pos = nozzle.transform.position;
     }
-
 
     void Update()
     {
-
+        Moving(nozzle);
     }
 
     Vector3 Direction(float x, float y, float z)
@@ -27,16 +32,14 @@ public class PrinterTest : MonoBehaviour
         return nozzle.transform.position - to;
     }
 
-    IEnumerator NozzleXposMoving()
+    void Moving(GameObject obj)
     {
-        float xPos = nozzle.transform.position.x;
-        
-        
-        if (xPos < xPosRange)
-        {
+        Vector3 v = obj.transform.position;
 
-        }
+        v.z = nozzleOriginPos.z + zRange * Mathf.Sin(Time.time * nozzleSpeed);
 
-        yield return null;
+        obj.transform.position = v;
+
+
     }
 }
