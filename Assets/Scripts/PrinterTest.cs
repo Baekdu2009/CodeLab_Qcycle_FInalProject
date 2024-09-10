736f2871f2b47a217b123c474f03e33b1ac70809
Baekdu2009 <cik7353@gmail.com>
2024-09-10 13:04:50 +0900

    0910_1300; feat. 프린터 적층 컨셉 추가

--- Assets/Scripts/PrinterTest.cs
+++ Assets/Scripts/PrinterTest.cs
@@ -7,6 +7,10 @@ public class PrinterTest : MonoBehaviour
 {
     // 움직임 제한 범위 (x, y, z) = (-0.5 ~ 0.5, 0.6 ~ 1.15, -0.4 ~ 0.4)
     public GameObject nozzle;
+    public Transform nozzleTip;
+    public GameObject powderPrefab;
+    GameObject powderItem;
+    public GameObject powderObj { get => powderItem; }
     Vector3 nozzleOriginPos;
     Vector3 pos;
     public float nozzleSpeed;
@@ -14,6 +18,7 @@ public class PrinterTest : MonoBehaviour
     float yMin = 0.6f;
     float yMax = 1.15f;
     float zRange = 0.4f;
+    Coroutine itemCreationCoroutine;
 
     void Start()
     {
@@ -23,23 +28,94 @@ public class PrinterTest : MonoBehaviour
 
     void Update()
     {
-        Moving(nozzle);
+        KeyInputMoving();
     }
 
-    Vector3 Direction(float x, float y, float z)
+    //Vector3 Direction(float x, float y, float z)
+    //{
+    //    Vector3 to = new Vector3(x, y, z);
+    //    return nozzle.transform.position - to;
+    //}
+
+    //void Moving(GameObject obj)
+    //{
+    //    Vector3 v = obj.transform.position;
+
+    //    v.z = nozzleOriginPos.z + zRange * Mathf.Sin(Time.time * nozzleSpeed);
+
+    //    obj.transform.position = v;
+    //}
+
+    void KeyInputMoving()
     {
-        Vector3 to = new Vector3(x, y, z);
-        return nozzle.transform.position - to;
+        Vector3 nozzleVector = nozzle.transform.position;
+
+        // A키와 D키로 z축 이동
+        if (Input.GetKey(KeyCode.A))
+        {
+            nozzleVector.z -= nozzleSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            nozzleVector.z += nozzleSpeed * Time.deltaTime;
+        }
+
+        // W키와 S키로 x축 이동
+        if (Input.GetKey(KeyCode.W))
+        {
+            nozzleVector.x -= nozzleSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            nozzleVector.x += nozzleSpeed * Time.deltaTime;
+        }
+
+        // 위쪽 화살표와 아래쪽 화살표로 y축 이동
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            nozzleVector.y += nozzleSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            nozzleVector.y -= nozzleSpeed * Time.deltaTime;
+        }
+
+        // Space 키를 눌렀을 때 오브젝트 생성 시작/중지
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (itemCreationCoroutine == null)
+            {
+                itemCreationCoroutine = StartCoroutine(ItemCreation());
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (itemCreationCoroutine != null)
+            {
+                StopCoroutine(itemCreationCoroutine);
+                itemCreationCoroutine = null;
+            }
+        }
+
+        // 움직임의 범위를 제한합니다.
+        nozzleVector.x = Mathf.Clamp(nozzleVector.x, nozzleOriginPos.x - xRange, nozzleOriginPos.x + xRange);
+        nozzleVector.y = Mathf.Clamp(nozzleVector.y, yMin, yMax);
+        nozzleVector.z = Mathf.Clamp(nozzleVector.z, nozzleOriginPos.z - zRange, nozzleOriginPos.z + zRange);
+
+        // 최종 위치를 업데이트합니다.
+        nozzle.transform.position = nozzleVector;
     }
 
-    void Moving(GameObject obj)
+    IEnumerator ItemCreation()
     {
-        Vector3 v = obj.transform.position;
+        while (true) // 무한 루프를 통해 계속 생성
+        {
+            powderItem = Instantiate(powderPrefab, nozzleTip);
+            powderItem.transform.parent = null;
 
-        v.z = nozzleOriginPos.z + zRange * Mathf.Sin(Time.time * nozzleSpeed);
-
-        obj.transform.position = v;
-
-
+            yield return new WaitForSeconds(0.2f); // 1초 대기
+        }
     }
+
+
 }
---
+++ Assets/Scripts/powder.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Powder : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Plate") || collision.gameObject.CompareTag("Powder"))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                //rb.freezeRotation = true; // 회전 고정
+                rb.constraints = RigidbodyConstraints.FreezeAll; // 위치 고정
+                //rb.isKinematic = true;
+            }
+        }
+    }
+}
