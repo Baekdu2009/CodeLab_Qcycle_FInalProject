289b0111788c079e3d67ae59db2d3ef7610fc425
Baekdu2009 <cik7353@gmail.com>
2024-09-27 18:11:37 +0900

    0927_1810; feat. 프린터 변경

---
+++ Assets/Script/Manager1.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Manager1 : MonoBehaviour
+{
+    public GameObject StartingPanel;
+    public GameObject FilamentFactory;
+    public GameObject PrinterFactory;
+    public GameObject AGVcontrol;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
