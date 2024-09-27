9bbcbb3efecbfda856298c619f98b2d7c2ce3e5d
ChiantiWine <hsm11256@naver.com>
2024-09-27 11:41:31 +0900

    2024.09.27
    오전
    Filament1, GameManager1 수정 완료
    Filament2, GameManager2 수정 중

---
+++ Assets/Scr/Filament_increace2.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Filament_increace2 : MonoBehaviour
+{
+    public float initialScale = 0.001f; // 초기 Scale
+    public float scaleIncreaseSpeed; // 길이 증가 속도
+    private bool isScaling = true;  // Scale 증가 여부
+    public float maxScale = 1.65f; // 최대 Scale
+    public float xMoveSpeed; // X축 이동 속도
+
+    void Start()
+    {
+        // 초기 Scale 설정
+        transform.localScale = new Vector3(initialScale, initialScale, initialScale);
+    }
+
+    void Update()
+    {
+        if (isScaling)
+        {
+            // 실린더의 Y축 Scale을 증가시켜 길이 늘리기
+            Vector3 currentScale = transform.localScale;
+            currentScale.y += scaleIncreaseSpeed * Time.deltaTime; // Y축 Scale 증가 양쪽으로 증가
+            transform.position += new Vector3(scaleIncreaseSpeed * Time.deltaTime, 0, 0); // 위치 조정 (X축으로 이동)
+
+            // Y축 Scale이 1.65 초과 시 제한
+            if (currentScale.y > maxScale)
+            {
+                currentScale.y = maxScale; // 최대 Scale로 설정
+                isScaling = false;    // Scale 증가 중지
+                GameManager2.instance.SpawnPrefab1();
+            }
+
+            // 새로운 Scale 적용(매 프레임마다 변화하는 스케일 적용)
+            transform.localScale = currentScale;
+        }
+    }
+}
---
+++ Assets/Scr/GameManager2.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameManager2 : MonoBehaviour
+{
+    public static GameManager2 instance;
+    public GameObject Filament; // Filament 프리팹
+    public GameObject Filamentdia; // 대각선 Filament 프리팹
+    public GameObject Filament2;  // Filament2 프리팹
+
+    public float scaleIncreaseSpeed; // 길이 증가 속도
+    float delayTime = 2.0f;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+    }
+
+    void Start()
+    {
+        // (0, 0, 0) 위치에 Z축으로 90도 회전하여 Filament 생성
+        Quaternion rotation = Quaternion.Euler(0, 0, 90); // Z축으로 90도 회전
+
+        // GameManager의 위치에서 Filament 생성
+        GameObject FilamentIn = Instantiate(Filament, transform.position, rotation);
+
+        // 필요시 주석 제거
+        // CylinderController 스크립트를 추가하여 초기화
+         /*Filament_increace controller = FilamentIn.AddComponent<Filament_increace>();
+         controller.scaleIncreaseSpeed = scaleIncreaseSpeed;     // 길이 증가 속도 설정*/
+       
+        
+        // 26.5 1.7 -6.39 rotate z -50
+        // 27.0601 2.0876 -6.39 rotate z 90 Scale 0.2067529
+    }
+
+    public void SpawnPrefab1()
+    {
+        if (Filamentdia != null)
+        {
+            // 생성할 위치 및 회전
+            Vector3 SpawnPosition = new Vector3(26.7042f, 1.90857f, -6.39f);
+            Quaternion SpawnRotation = Quaternion.Euler(0, 0, -50);
+
+            // Filamentdia 생성
+            GameObject FilamentdiaIn = Instantiate(Filamentdia, SpawnPosition, SpawnRotation);
+            Debug.Log("Filamentdia 생성");
+
+            StartCoroutine(SpawnWithDelay());
+        }
+    }
+
+    public IEnumerator SpawnWithDelay()
+    {
+        if (Filament2 != null)
+        {
+            // 생성할 위치 및 회전
+            Vector3 SpawnPosition2 = new Vector3(27.135f, 2.0876f, -6.39f);
+            Quaternion SpawnRotation2 = Quaternion.Euler(0, 0, 90);
+
+            // Filament2 생성
+            GameObject Filament2In = Instantiate(Filament2, SpawnPosition2, SpawnRotation2);
+            Debug.Log("Filament2 생성");
+
+            yield return new WaitForSeconds(delayTime);
+
+        }
+    }
+}
