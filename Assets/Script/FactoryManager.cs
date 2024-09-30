5a23b969a2169a5f3066e6127db315774945eac1
Baekdu2009 <cik7353@gmail.com>
2024-09-30 23:17:14 +0900

    0930_2315; feat. 필라멘트 회전 추가
    필라멘트 회전하며 스케일 조절되는 부분 추가로 수정해야함

--- Assets/Script/FactoryManager.cs
+++ Assets/Script/FactoryManager.cs
@@ -5,14 +5,14 @@ public class FactoryManager : MonoBehaviour
     public GameObject SelectionPanel;
     public GameObject FilamentFactory;
     public GameObject PrinterFactory;
-    public GameObject AGVcontrol;
+    // public GameObject AGVcontrol;
 
     void Start()
     {
         SelectionPanel.SetActive(true);
         FilamentFactory.SetActive(false);
         PrinterFactory.SetActive(false);
-        AGVcontrol.SetActive(false);
+        // AGVcontrol.SetActive(false);
     }
 
     void Update()
@@ -25,27 +25,27 @@ public class FactoryManager : MonoBehaviour
         SelectionPanel.SetActive(true);
         FilamentFactory.SetActive(false);
         PrinterFactory.SetActive(false);
-        AGVcontrol.SetActive(false);
+        //AGVcontrol.SetActive(false);
     }
     public void BtnFilamentFactory()
     {
         SelectionPanel.SetActive(false);
         FilamentFactory.SetActive(true);
         PrinterFactory.SetActive(false);
-        AGVcontrol.SetActive(false);
+        //AGVcontrol.SetActive(false);
     }
     public void BtnPrinterFactory()
     {
         SelectionPanel.SetActive(false);
         FilamentFactory.SetActive(false);
         PrinterFactory.SetActive(true);
-        AGVcontrol.SetActive(false);
+        //AGVcontrol.SetActive(false);
     }
     public void BtnAGVControl()
     {
         SelectionPanel.SetActive(false);
         FilamentFactory.SetActive(false);
         PrinterFactory.SetActive(false);
-        AGVcontrol.SetActive(true);
+        //AGVcontrol.SetActive(true);
     }
 }
--- Assets/Script/FilamentFactory.cs
+++ Assets/Script/FilamentFactory.cs
@@ -1,9 +1,15 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FilamentFactory : MonoBehaviour
 {
+    public Transform filamentRotPosition;
+    public GameObject filamentCover;
+    public GameObject filamentLine;
+    private GameObject filamentObject;
+
     public GameObject Canvas;
     public Image conveyorStatus;
     public Image shredderStatus;
@@ -16,30 +22,40 @@ public class FilamentFactory : MonoBehaviour
     public TMP_Text tank1Text;
     public TMP_Text tank2Text;
 
-    public bool conveyorWorkWell;
-    public bool shredderWorkWell;
-    public bool extruder1WorkWell;
-    public bool wirecuttingWorkWell;
-    public bool screwconveyorWorkWell;
-    public bool extruder2WorkWell;
-    public bool rollingWorkWell;
-    public bool spoolerWorkWell;
+    float rotSpeed = 200f;
+    Vector3 initialScale;
+    float currentRotation = 0f;
 
-    public bool conveyorStop;
-    public bool shredderStop;
-    public bool extruder1Stop;
-    public bool wirecuttingStop;
-    public bool screwconveyorStop;
-    public bool extruder2Stop;
-    public bool rollingStop;
-    public bool spoolerStop;
-    
-    void Start()
-    {
-        
-    }
+    // 각 장비 상태 변수
+    bool conveyorWorkWell = true;
+    bool shredderWorkWell = true;
+    bool extruder1WorkWell = true;
+    bool wirecuttingWorkWell = true;
+    bool screwconveyorWorkWell = true;
+    bool extruder2WorkWell = true;
+    bool rollingWorkWell = true;
+    bool spoolerWorkWell = true;
+
+    // 각 장비 정지 상태 변수
+    bool conveyorStop = false;
+    bool shredderStop = false;
+    bool extruder1Stop = false;
+    bool wirecuttingStop = false;
+    bool screwconveyorStop = false;
+    bool extruder2Stop = false;
+    bool rollingStop = false;
+    bool spoolerStop = false;
+
+    void Start() { }
 
     void Update()
+    {
+        UpdateStatus();
+        UpdateTankLevels();
+        HandleFilament();
+    }
+
+    private void UpdateStatus()
     {
         StatusCheck(conveyorStatus, conveyorWorkWell, conveyorStop);
         StatusCheck(shredderStatus, shredderWorkWell, shredderStop);
@@ -49,31 +65,67 @@ public class FilamentFactory : MonoBehaviour
         StatusCheck(extruder2Status, extruder2WorkWell, extruder2Stop);
         StatusCheck(rollingStatus, rollingWorkWell, rollingStop);
         StatusCheck(spoolerStatus, spoolerWorkWell, spoolerStop);
+    }
+
+    private void UpdateTankLevels()
+    {
         TankLevelCheck(tank1Text, 1, 10);
         TankLevelCheck(tank2Text, 2, 20);
     }
+
     private void StatusCheck(Image image, bool work, bool stop)
     {
-        if (work)
-        {
-            if (!stop)
-            {
-                image.color = Color.green;
-            }
-            else
-            {
-                
-                image.color = Color.yellow;
-            }
-        }
-        else
-        {
-            image.color = Color.red;
-        }
+        image.color = work ? (stop ? Color.yellow : Color.green) : Color.red;
     }
 
     private void TankLevelCheck(TMP_Text text, int tankNum, int tankLevel)
     {
         text.text = $"Tank{tankNum} Level {tankLevel:D2}%";
+    }
+
+    public void FilamentCreate()
+    {
+        if (filamentObject == null)
+        {
+            GameObject line = Instantiate(filamentLine);
+            GameObject cover = Instantiate(filamentCover);
+            
+            filamentObject = new GameObject("FilamentObject");
+            line.transform.parent = filamentObject.transform;
+            cover.transform.parent = filamentObject.transform;
+
+            filamentObject.transform.position = filamentRotPosition.position;
+            filamentObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+            initialScale = filamentObject.transform.localScale;
+            filamentObject.transform.localScale = new Vector3(0.1f, 1, 0.1f);
+        }
+    }
+
+    private void HandleFilament()
+    {
+        if (filamentObject != null)
+        {
+            FilamentScale();
+        }
+    }
+
+    private void FilamentScale()
+    {
+        float rotationThisFrame = rotSpeed * Time.deltaTime;
+        filamentObject.transform.Rotate(0, 0, rotationThisFrame);
+
+        currentRotation += rotationThisFrame;
+
+        if (currentRotation >= 360f)
+        {
+            currentRotation = 0;
+            filamentObject.transform.localScale += new Vector3(0.3f, 0.1f, 0.3f);
+        }
+
+        if (filamentObject.transform.localScale.z >= initialScale.z * 10f)
+        {
+            print("필라멘트 크기가 10배가 되었음");
+            rotSpeed = 0;
+        }
     }
 }
