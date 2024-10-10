b5fd4398fbee2dfa7b610c5dd792ba1d5bd1a03a
Baekdu2009 <cik7353@gmail.com>
2024-10-10 14:38:51 +0900

    카메라 움직임 추가

---
+++ Assets/Script/Interface/CameraController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 2f;
+
+    private float rotationX = 0f;
+
+    void Update()
+    {
+        // 키보드 입력 처리
+        float moveHorizontal = Input.GetAxis("Horizontal"); // A, D 키
+        float moveVertical = Input.GetAxis("Vertical"); // W, S 키
+
+        Vector3 moveDirection = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
+        Vector3 move = transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime;
+        transform.position += move;
+
+        // 마우스 입력 처리
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f); // 상하 회전 제한
+
+        transform.localEulerAngles = new Vector3(rotationX, transform.localEulerAngles.y + mouseX, 0f);
+    }
+}
