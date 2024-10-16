d8a32759a98e7b41e9584fe8f4762b2d50d41c60
ChiantiWine <hsm11256@naver.com>
2024-10-16 14:02:25 +0900

    2024.10.16
    Box Taping 수정

--- Assets/Script/Boxing/Taping.cs
+++ Assets/Script/Boxing/Taping.cs
@@ -8,20 +8,47 @@ public class Taping : MonoBehaviour
     {
         if (other.CompareTag("Box"))
         {
-            // Tape prefab 생성
-            GameObject tape = Instantiate(tapePrefab);
+            if (gameObject.name == "Taping")
+            {
 
-            // Tape의 크기 설정
-            tape.transform.localScale = new Vector3(0.82f, 2f, 2f);
+                BackTapeSqwan(other.gameObject);
+            }
 
-            // 박스를 부모로 설정
-            tape.transform.SetParent(other.transform);
-
-            // Tape의 상대 위치로 설정
-            tape.transform.localPosition = new Vector3(0, -0.111f, 0.094f);
-            tape.transform.localRotation = Quaternion.Euler(180, 0, -90);
+            if (gameObject.name == "Taping2")
+            {
+                FrontTapeSqwan(other.gameObject);
+            }
         }
     }
+    private void FrontTapeSqwan(GameObject obj)
+    {
+        // Tape prefab 생성
+        GameObject tape = Instantiate(tapePrefab);
 
+        // Tape의 크기 설정
+        tape.transform.localScale = new Vector3(0.82f, 2f, 2f);
 
+        // 박스를 부모로 설정
+        tape.transform.SetParent(obj.transform);
+
+        // Tape의 상대 위치로 설정
+        tape.transform.localPosition = new Vector3(0, -0.111f, -0.0018f);
+        tape.transform.localRotation = Quaternion.Euler(180, 180, -90);
+    }
+
+    private void BackTapeSqwan(GameObject obj)
+    {
+        // Tape prefab 생성
+        GameObject tape = Instantiate(tapePrefab);
+
+        // Tape의 크기 설정
+        tape.transform.localScale = new Vector3(0.82f, 2f, 2f);
+
+        // 박스를 부모로 설정
+        tape.transform.SetParent(obj.transform);
+
+        // Tape의 상대 위치로 설정
+        tape.transform.localPosition = new Vector3(0, -0.111f, 0.094f);
+        tape.transform.localRotation = Quaternion.Euler(180, 0, -90);
+    }
 }
