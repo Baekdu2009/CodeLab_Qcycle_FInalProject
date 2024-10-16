d7f025af1908710d71280c0ce549b8df08d31cd4
yunsangho <ysh0023@naver.com>
2024-10-16 11:30:42 +0900

    20241016

--- Assets/Script/Connect/TCPClient.cs
+++ Assets/Script/Connect/TCPClient.cs
@@ -99,7 +99,7 @@ public class TCPClient : MonoBehaviour
 
         // 문자열을 분리
         string[] strSplited = dataFromServer.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-        //print("2. ReadDeviceBlock newData: " + string.Join(",", strSplited));
+       print("2. ReadDeviceBlock newData: " + string.Join(",", strSplited));
 
         // 배열의 크기를 strSplited의 길이에 맞추기
         int[] values = new int[strSplited.Length];
@@ -158,7 +158,7 @@ public class TCPClient : MonoBehaviour
         int extruder2SensorValue = (extruderSensor[1].isSensing == true) ? 1 : 0;
         int pressure1SensorValue = (pressureSensor[0].isPressing == true) ? 1 : 0;
         int pressure2SensorValue = (pressureSensor[1].isPressing == true) ? 1 : 0;
-        int ls1value = (tankSensor[0].isDetected == true) ? 1 : 0;
+        int ls1value = (filamentFactory.limiting == true) ? 1 : 0;
 
         int sensorNum = pressure2SensorValue * 32 + pressure1SensorValue*16 +extruder2SensorValue*8 +extruder1SensorValue * 4 + tank2SensorValue*2 + tank1SensorValue * 1;
         int lsNum = ls1value * 1;
@@ -198,7 +198,7 @@ public class TCPClient : MonoBehaviour
             {
                 for (int j = 0; j < pointY[i].Length; j++)
                 {
-                    print($"PointY[{i}][{j}]값 : {pointY[i][j]}");
+                   // print($"PointY[{i}][{j}]값 : {pointY[i][j]}");
                 }
             }
 
@@ -285,6 +285,8 @@ public class TCPClient : MonoBehaviour
             {
                
                 linemanagers[1].isOn = true;
+
+
                 
             }
             else if (runExtruder2 != 1)
@@ -345,6 +347,14 @@ public class TCPClient : MonoBehaviour
             else if (pressureSensor[1].isPressing == false)
             {
                 Extruder2Pressure = 0;
+            }
+            if(filamentFactory.limiting == true)
+            {
+                limitSwitch1 = 1;
+            }
+            else if (filamentFactory.limiting == false)
+            {
+                limitSwitch1 = 0;
             }
 
         }
